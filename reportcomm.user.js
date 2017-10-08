961fccc3cf2c50b2ede269d2ff985111e56b88ff
MilkGames <MilkGames@users.noreply.github.com>
2017-10-08 17:06:23 +0200

    Steam dialog

--- reportcomm.user.js
+++ reportcomm.user.js
@@ -26,11 +26,12 @@
     }
 
     jQuery(".repcomment").click(function() {
-        var reason = prompt("Please enter the reason for reporting this comment.", "");
-        if (reason) {
-            var $this = jQuery(this);
-            var author = $this.parent().find(".commentthread_author_link").attr("data-miniprofile");
-            var date = $this.parent().find(".commentthread_comment_timestamp").attr("title");
+        var $this = jQuery(this);
+        var author = $this.parent().find(".commentthread_author_link").attr("data-miniprofile");
+        var date = $this.parent().find(".commentthread_comment_timestamp").attr("title");
+        ShowPromptDialog( "Please enter the reason for reporting this comment.", "Report abusive comments to the moderators.", "Submit Report", "Cancel")
+        .done(function(reason, other) {
+		    if (reason) {
             // Ugly code pls no to kill
             var datatosend = "comment=Author: " + author + "\n" + date + "\nReason: " + reason + "&count=15&sessionid=" + g_sessionID + "&extended_data=%7B%22topic_permissions%22%3A%7B%22can_view%22%3A1%2C%22can_post%22%3A1%2C%22can_reply%22%3A1%2C%22can_moderate%22%3A1%2C%22can_edit_others_posts%22%3A1%2C%22can_purge_topics%22%3A1%2C%22is_banned%22%3A0%2C%22can_delete%22%3A1%2C%22can_edit%22%3A1%7D%2C%22original_poster%22%3A144601510%2C%22forum_appid%22%3A0%2C%22forum_public%22%3A0%2C%22forum_type%22%3A%22General%22%2C%22forum_gidfeature%22%3A%220%22%7D&feature2=1488861734111023805&oldestfirst=true&include_raw=true";
             var xhr = new XMLHttpRequest();
@@ -46,5 +47,6 @@
             //Update the Report number on top of page. Live updates whooooo!11
             // IT WORKS! ... I hope.
         }
+        });
     });
 })();
