e6a32bc3a997c33b99b0c79c253082e037ff6a16
MilkGames <MilkGames@users.noreply.github.com>
2017-11-05 00:13:06 +0100

    fix #4
    also now posts in a different, cleaned thread. and lets the user know if something went wrong with the request.

--- reportcomm.user.js
+++ reportcomm.user.js
@@ -40,21 +40,29 @@
         .done(function(reason, other) {
 		    if (reason) {
             // Ugly code pls no to kill
-            var datatosend = "comment=Author: " + author + "\n" + date + "\nReason: " + reason + "&count=15&sessionid=" + g_sessionID + "&extended_data=%7B%22topic_permissions%22%3A%7B%22can_view%22%3A1%2C%22can_post%22%3A1%2C%22can_reply%22%3A1%2C%22can_moderate%22%3A1%2C%22can_edit_others_posts%22%3A1%2C%22can_purge_topics%22%3A1%2C%22is_banned%22%3A0%2C%22can_delete%22%3A1%2C%22can_edit%22%3A1%7D%2C%22original_poster%22%3A144601510%2C%22forum_appid%22%3A0%2C%22forum_public%22%3A0%2C%22forum_type%22%3A%22General%22%2C%22forum_gidfeature%22%3A%220%22%7D&feature2=1488861734111023805&oldestfirst=true&include_raw=true";
+            var datatosend = "comment=Author: " + author + "\n" + date + "\nReason: " + reason + "&count=15&sessionid=" + g_sessionID + "&extended_data=%7B%22topic_permissions%22%3A%7B%22can_view%22%3A1%2C%22can_post%22%3A1%2C%22can_reply%22%3A1%2C%22can_moderate%22%3A1%2C%22can_edit_others_posts%22%3A1%2C%22can_purge_topics%22%3A1%2C%22is_banned%22%3A0%2C%22can_delete%22%3A1%2C%22can_edit%22%3A1%7D%2C%22original_poster%22%3A144601510%2C%22forum_appid%22%3A0%2C%22forum_public%22%3A0%2C%22forum_type%22%3A%22General%22%2C%22forum_gidfeature%22%3A%220%22%7D&feature2=1479857071262320410&oldestfirst=true&include_raw=true";
             var xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                if (xhr.readyState === xhr.DONE) { // The request is done, we have a response from the server.
+                    if (xhr.responseText === '{"success":false}') { // Is the response text {"success":false}? Let the user know something went wrong.
+                        ShowAlertDialog('Reddit Steam Group Reporter', 'Whoops! It looks like something went wrong. Try again?');
+                    } else { // Else? It should be all fine :)
+                        ShowAlertDialog('Thank You!', 'Thank you for reporting a comment to the Reddit Group Moderators. \nYou may view your report <a href="http://steamcommunity.com/groups/reddit/discussions/0/1488861734111023805">here</a>.');
+                        localStorage.setItem('reports',parseInt(reports)+1);
+                        //Make sure to parse it as an int, not a string, otherwise you get 01, 011, 0111, etc.
+                        localStorage.setItem(dateu,"");
+                        //Makes a localStorage item with the Unix Timestamp - prevent doublereports?
+                        reports = localStorage.getItem("reports");
+                        element.innerHTML = "Reports: "+reports;
+                        //Update the Report number on top of page. Live updates whooooo!11
+                        // IT WORKS! ... I hope.
+                    }
+                }
+            };
             xhr.withCredentials = true;
             xhr.open("POST", "http://steamcommunity.com/comment/ForumTopic/post/103582791429796426/864943227215847264/");
             xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
             xhr.send(datatosend);
-            ShowAlertDialog('Thank You!', 'Thank you for reporting a comment to the Reddit Group Moderators. \nYou may view your report <a href="http://steamcommunity.com/groups/reddit/discussions/0/1488861734111023805">here</a>.');
-            localStorage.setItem('reports',parseInt(reports)+1);
-            //Make sure to parse it as an int, not a string, otherwise you get 01, 011, 0111, etc.
-            localStorage.setItem(dateu,"");
-            //Makes a localStorage item with the Unix Timestamp - prevent doublereports?
-            reports = localStorage.getItem("reports");
-            element.innerHTML = "Reports: "+reports;
-            //Update the Report number on top of page. Live updates whooooo!11
-            // IT WORKS! ... I hope.
         }
         });
     });
