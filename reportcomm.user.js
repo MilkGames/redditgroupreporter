699ead8dfd91071cfade7688b5d24ee73f51f365
MilkGames <memegames1337@gmail.com>
2018-02-09 19:47:05 +0100

    update on situation

--- reportcomm.user.js
+++ reportcomm.user.js
@@ -9,29 +9,32 @@
 // ==/UserScript==
 
 (function() {
-    if (!localStorage.getItem("noticeok")) { // If localStorage doesn't have item notice ok
-        ShowAlertDialog( "Reddit Steam Group Reporter", "<h3>Message from Reddit Group Reporter developers:</h3>\nA Chrome extension is currently in development. I'm not certain when this extension will release but I wanted to give a headsup before this userscript suddenly stops working. A Firefox extension should also be released at the same time. \n\n<h1>Thank you for understanding.</h1>", "Understood" )
+    if (localStorage.getItem("noticeok")) {
+        localStorage.removeItem("noticeok");
+    }
+    if (!localStorage.getItem("notice2ok")) {
+        ShowAlertDialog( "Reddit Steam Group Reporter", "<h3>Message from Reddit Group Reporter developers:</h3>\nStatus of Chrome extension: Errors, errors, and errors. I've been dealing with a bunch of errors mainly in relation with the grabbing of cookies. If anyone would like to help me out, <a href='steam://friends/add/76561198104867238'>please do</a> or add me via Discord (MilkGames#6495) \n\n\n<img height='450' width='850' src='https://milkgames.pw/file/8FrI.gif'>\n\n<h1>Thank you for understanding.</h1>", "Understood" )
         .done(function() {
-        localStorage.setItem("noticeok", "true"); // Show notice & if done (ok button pressed) set localStorage item noticeok to true.
+        localStorage.setItem("notice2ok", "true"); // Show notice & if done (ok button pressed) set localStorage item noticeok to true.
         }
     );}
     var element = document.createElement("reportcount");
-	var reports = localStorage.getItem("reports");
-	if (reports === null) {
-		localStorage.setItem("reports", "0");
-		//Since reports was originally "null", and we need to be userfriendly, we set the reports to the number "0", and update the var.
-		reports = localStorage.getItem("reports");
-	}
+    var reports = localStorage.getItem("reports");
+    if (reports === null) {
+        localStorage.setItem("reports", "0");
+        //Since reports was originally "null", and we need to be userfriendly, we set the reports to the number "0", and update the var.
+        reports = localStorage.getItem("reports");
+    }
     element.appendChild(document.createTextNode('Reports: ' + reports));
     document.getElementsByClassName('commentthread_count')[0].appendChild(element);
-	jQuery("reportcount").css( {
-	   'color' : '#d65d5d',
- 	   'border' : '1px solid #565656',
-	   'border-radius': '10px',
-   	   'padding' : '3px',
-	   'padding-left' : '4px',
-	   'padding-right' : '4px',
-	   'height' : '10px' } );
+    jQuery("reportcount").css( {
+       'color' : '#d65d5d',
+       'border' : '1px solid #565656',
+       'border-radius': '10px',
+       'padding' : '3px',
+       'padding-left' : '4px',
+       'padding-right' : '4px',
+       'height' : '10px' } );
     var $comments = jQuery("[class*='commentthread_comment_timestamp']");
     if ($comments.length > 0) {
         $comments.after(' <a class="actionlink repcomment">Report Comment</a><a class="actionlink">');
@@ -44,7 +47,7 @@
         var dateu = $this.parent().find(".commentthread_comment_timestamp").attr("data-timestamp");
         ShowPromptDialog( "Reporting a Comment", "Please enter your reasoning for reporting this comment", "Submit Report", "Cancel")
         .done(function(reason, other) {
-		    if (reason) {
+            if (reason) {
             // Ugly code pls no to kill
             var datatosend = "comment=Author: " + author + "\n" + date + "\nReason: " + reason + "&count=15&sessionid=" + g_sessionID + "&extended_data=%7B%22topic_permissions%22%3A%7B%22can_view%22%3A1%2C%22can_post%22%3A1%2C%22can_reply%22%3A1%2C%22can_moderate%22%3A1%2C%22can_edit_others_posts%22%3A1%2C%22can_purge_topics%22%3A1%2C%22is_banned%22%3A0%2C%22can_delete%22%3A1%2C%22can_edit%22%3A1%7D%2C%22original_poster%22%3A144601510%2C%22forum_appid%22%3A0%2C%22forum_public%22%3A0%2C%22forum_type%22%3A%22General%22%2C%22forum_gidfeature%22%3A%220%22%7D&feature2=1479857071262320410&oldestfirst=true&include_raw=true";
             var xhr = new XMLHttpRequest();
